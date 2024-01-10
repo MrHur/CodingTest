914e553d1a70a1f87cceefe2581c24d364060cb4
Greedy92 <79696786+MrHur@users.noreply.github.com>
2024-01-10 21:14:06 +0900

    [Bronze V] Title: 개, Time: 108 ms, Memory: 13848 KB -BaekjoonHub

--- 백준/Bronze/10172. 개/개.java
+++ 백준/Bronze/10172. 개/개.java
@@ -1,5 +1,13 @@
-public class Main {
+public class Main { 
     public static void main(String[] args){
-        System.out.print("|\\_/|\n|q p|   /}\n( 0 )\"\"\"\\\n|\"^\"`    |\n||_/=\\\\__|");  
-    }
+    System.out.println("|\\_/|"); 
+    System.out.println("|q p|   /}");
+    System.out.println("( 0 )\"\"\"\\");
+    System.out.println("|\"^\"`    |");
+    System.out.println("||_/=\\\\__|");
+    }                                                     
 }
+
+
+
+
