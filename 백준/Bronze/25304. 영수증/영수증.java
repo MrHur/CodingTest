3b1100e4be8c9693d82d0349fef59137831b2baa
Greedy92 <79696786+MrHur@users.noreply.github.com>
2024-01-11 15:00:18 +0900

    [Bronze IV] Title: 영수증, Time: 224 ms, Memory: 17732 KB -BaekjoonHub

---
+++ 백준/Bronze/25304. 영수증/영수증.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int X = sc.nextInt();
+		int[] N = new int[sc.nextInt()];
+		int a = 0;
+		int b = 0;
+		int sum = 0;
+
+		while (true) {
+			for (int i = 0; i < N.length; i++) {
+				a = sc.nextInt();
+				b = sc.nextInt();
+				sum += a * b;
+
+//				System.out.println(N.length);
+			}
+//			System.out.println(sum);
+//			System.out.println(X);
+//			System.out.println(N.length);
+//			System.out.println(b);
+			if (X == sum) {
+				System.out.println("Yes");
+			} else {
+				System.out.println("No");
+			}
+			break;
+		}
+		// 스캐너를 닫음.
+		sc.close();
+	}
+}
