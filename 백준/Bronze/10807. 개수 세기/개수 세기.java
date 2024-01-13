b2a293055f4e02d1f31f122041e4ed8fcbcc7021
Greedy92 <79696786+MrHur@users.noreply.github.com>
2024-01-13 12:18:31 +0900

    [Bronze V] Title: 개수 세기, Time: 220 ms, Memory: 17816 KB -BaekjoonHub

---
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int count=0;
+//		System.out.println("N의 갯수");
+//		System.out.println(N);
+		int[] A = new int[N];
+//		System.out.println("A 배열의 길이");
+//		System.out.println(A.length);
+		for(int i = 0; i<A.length; i++) {
+			A[i]= sc.nextInt();
+		}
+		int X = sc.nextInt();
+		for(int i = 0; i<A.length; i++) {
+			if (A[i]==X) {
+				count++;
+			}
+		}
+
+		System.out.println(count);
+		sc.close();
+	}
+}
