6d5036228f1d1ed43040f795a0a1a0ab8f257603
Greedy92 <79696786+MrHur@users.noreply.github.com>
2024-01-14 15:49:24 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 136 ms, Memory: 14344 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));    
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); 
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        
+        for(int i=0; i<N; i++){
+            for(int j=0; j<=N-i-2; j++){
+                bw.write(" ");
+            }
+            for(int j=0; j<=i; j++){
+                bw.write("*");
+            }bw.newLine();
+        }
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
