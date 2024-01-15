658cf94f1b5f0d101e86613e3340e94446ec5515
Greedy92 <79696786+MrHur@users.noreply.github.com>
2024-01-15 15:09:16 +0900

    [Bronze II] Title: 공 바꾸기, Time: 240 ms, Memory: 18532 KB -BaekjoonHub

---
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int[] N = new int[sc.nextInt()];
+		int M = sc.nextInt();
+
+		int j;
+		int c = 0;
+		for (int i = 0; i < N.length; i++) {
+			N[i] = i + 1;
+		}
+		for (j = 0; j < M; j++) {
+			int a = sc.nextInt(); // 1
+			int b = sc.nextInt(); // 3
+			c = N[a - 1];
+			N[a - 1] = N[b - 1];
+			N[b - 1] = c;
+		}
+		for (j = 0; j < N.length; j++) {
+			System.out.print(N[j] + " ");
+		}
+		// 스캐너를 닫음.
+		sc.close();
+	}
+}
