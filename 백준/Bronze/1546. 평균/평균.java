b0f8179712d5c51b0ae0b859d861964153483bed
Greedy92 <79696786+MrHur@users.noreply.github.com>
2024-01-17 08:57:59 +0900

    [Bronze I] Title: 평균, Time: 276 ms, Memory: 18768 KB -BaekjoonHub

---
+++ 백준/Bronze/1546. 평균/평균.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int[] N = new int[sc.nextInt()];
+
+		double sum = 0;
+		double avg = 0;
+		for (int i = 0; i < N.length; i++) {
+			N[i] = sc.nextInt();
+			sum += N[i];
+		}
+		avg = sum / N.length;
+		int max = 0;
+		double newAvg = 0;
+
+		for (int j = 0; j < N.length; j++) {
+			if (N[j] > max) {
+				max = N[j];
+			}
+			newAvg = avg / max * 100;
+		}
+
+		System.out.print(newAvg);
+
+		// 스캐너를 닫음.
+		sc.close();
+	}
+}
