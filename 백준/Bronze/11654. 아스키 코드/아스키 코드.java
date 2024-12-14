781868ad799e17317178ad6913d9a0759112dd54
Greedy92 <79696786+MrHur@users.noreply.github.com>
2024-12-15 00:42:36 +0900

    Update 아스키 코드.java

--- 백준/Bronze/11654. 아스키 코드/아스키 코드.java
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -1,11 +1,11 @@
 import java.util.Scanner;
 
 public class Main {
-	    public static void main(String[] args){
-	        Scanner sc = new Scanner(System.in);
-	        String n = sc.next();
-            sc.close();
-	        int a = n.charAt(0);
-	        System.out.println(a);	                
-	    }
+    public static void main(String[] args){
+	Scanner sc = new Scanner(System.in);
+	String n = sc.next();
+        sc.close();
+	int a = n.charAt(0);
+	System.out.println(a);	                
 	}
+}
