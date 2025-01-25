cf21b9db5113e665631aea08c7c0233c830d1311
Greedy92 <79696786+MrHur@users.noreply.github.com>
2025-01-26 03:10:36 +0900

    Update 빠른 A＋B.java

--- 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.java
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.java
@@ -6,7 +6,6 @@ import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main {
-
 	public static void main(String[] args) {
 		try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 				BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))){
