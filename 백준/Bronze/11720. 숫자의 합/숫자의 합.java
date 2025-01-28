bf8787e2f3779a88c48a69b8b6c5be3ab8e6da2f
Greedy92 <79696786+MrHur@users.noreply.github.com>
2025-01-29 00:10:15 +0900

    Update 숫자의 합.java

--- 백준/Bronze/11720. 숫자의 합/숫자의 합.java
+++ 백준/Bronze/11720. 숫자의 합/숫자의 합.java
@@ -3,10 +3,8 @@ import java.util.Scanner;
 public class Main{
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        
+        int N = sc.nextInt();        
         String a = sc.next();
-        
         int sum = 0;
         
         for(int i=0; i<N; i++){
