e93c8c7abda7e82e5534caf5608481e5708e7b06
Greedy92 <79696786+MrHur@users.noreply.github.com>
2025-07-02 00:03:39 +0900

    Update 문자와 문자열.java

--- 백준/Bronze/27866. 문자와 문자열/문자와 문자열.java
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.java
@@ -6,7 +6,6 @@ import java.io.OutputStreamWriter;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
-
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		String s = br.readLine();
