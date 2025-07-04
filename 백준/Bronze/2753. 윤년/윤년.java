3117d7bc99613cd6f3bc1896a333e1910cddf188
Greedy92 <79696786+MrHur@users.noreply.github.com>
2025-07-05 01:01:18 +0900

    Update 윤년.java

--- 백준/Bronze/2753. 윤년/윤년.java
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -4,7 +4,6 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int a = sc.nextInt();
-//		int b = sc.nextInt();
         if(((a%4==0)&&(a%100!=0))||((a%4==0)&&(a%400==0))){
             System.out.print(1);
         } else {System.out.print(0);}
