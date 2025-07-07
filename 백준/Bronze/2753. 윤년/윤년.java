5b9b74a08aa2b8b8fedbee60230a63b94ba47421
Greedy92 <79696786+MrHur@users.noreply.github.com>
2025-07-08 00:01:32 +0900

    Update 윤년.java

--- 백준/Bronze/2753. 윤년/윤년.java
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -6,8 +6,7 @@ public class Main {
 		int a = sc.nextInt();
         if(((a%4==0)&&(a%100!=0))||((a%4==0)&&(a%400==0))){
             System.out.print(1);
-        } else {System.out.print(0);}
-            		
+        } else {System.out.print(0);}            		
 		sc.close();
 	}
 }
