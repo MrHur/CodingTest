73cb49bb99673421b254f75a3ffc48dc144d8d3f
Greedy92 <79696786+MrHur@users.noreply.github.com>
2025-07-10 00:05:24 +0900

    Update 단어 길이 재기.java

--- 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -4,8 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String word = sc.nextLine();
-        System.out.print(word.length());
-        
+        System.out.print(word.length());       
         // Scanner 자원 해제
         sc.close();
     }
