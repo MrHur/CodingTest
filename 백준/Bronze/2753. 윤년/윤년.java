9f319a1933571666827d433130f4555f62c33e88
Greedy92 <79696786+MrHur@users.noreply.github.com>
2025-07-25 00:01:42 +0900

    Update 윤년.java

--- 백준/Bronze/2753. 윤년/윤년.java
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -2,10 +2,10 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	    Scanner sc = new Scanner(System.in);
 		int a = sc.nextInt();
 		
-        if(((a%4==0)&&(a%100!=0))||((a%4==0)&&(a%400==0))){
+            if(((a%4==0)&&(a%100!=0))||((a%4==0)&&(a%400==0))){
         	System.out.print(1);
         } else {System.out.print(0);}            		
 		sc.close();
