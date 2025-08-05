194f93819456d0370391edaf041eaf6bd5ddd1fc
Greedy92 <79696786+MrHur@users.noreply.github.com>
2025-08-06 00:01:06 +0900

    Update 최댓값.java

--- 백준/Bronze/2562. 최댓값/최댓값.java
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -8,7 +8,6 @@ import java.util.StringTokenizer;
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
- 
 		int[] arr = new int[9];
 		
 		for (int i = 0; i < 9; i++) {
