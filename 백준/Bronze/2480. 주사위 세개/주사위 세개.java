d48f0d02315f173b4e06f5e965c5c94be256132c
Greedy92 <79696786+MrHur@users.noreply.github.com>
2025-09-07 00:04:54 +0900

    Update 주사위 세개.java

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.java
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.java
@@ -33,7 +33,7 @@ public class Main {
 //        	dice3 가 가장 큰 경우.
 			} else if ((dice3 > dice1) && (dice1 > dice2)) {
 				System.out.print(dice3 * 100);
-//        		dice1 과 dice2가 dice3 보다 클 때, 
+//        	dice1 과 dice2가 dice3 보다 클 때, 
 			} else if ((dice1 > dice3) && (dice2 > dice3)) {
 //        		dice 1이 dice2보다 큰 경우.
 				if (dice1 > dice2) {
@@ -60,4 +60,5 @@ public class Main {
 		sc.close();
 	}
 }
+
 
